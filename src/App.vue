d153f78ff4aa3fe4a07f51a85c682eed620bffa3
GinoVi12 <gino.villena01@gmail.com>
2024-07-18 20:17:53 -0500

    Rick and morty init

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import PersonajesItem from "./components/PersonajesItem.vue";
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+
+    <!--
+  Objeto
+  [
+    {
+      id:1,
+      nombre: 'Paolo',
+      apellido : 'Villena'
+      }
+      {
+        id:2,
+        nombre:'Gino',
+        apellido:'Villena'
+        }
+        ]
+  -->
+
+    <PersonajesItem personaje="Rick" />
+    <PersonajesItem personaje="Morty" />
+    <PersonajesItem personaje="Beth" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/PersonajesItem.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+  personaje: String,
+});
+</script>
+
+<template>
+  <div>Hola soy el personaje {{ personaje }}</div>
+</template>
+
+<style></style>
